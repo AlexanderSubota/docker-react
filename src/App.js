3ee476b12780ba39ea614bdb8b806de4539837fb
Alexander Subota <alexander.subota.86@gmail.com>
2019-09-23 21:06:41 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hi there! I am Alexander!!!!!
         </a>
       </header>
     </div>
